863c5c7185a2c4a56152cbe3e701f69e955f0798
MarkM <muchanem@gmail.com>
2015-05-01 18:29:36 -0400

    save

---
+++ colors.sh
@@ -0,0 +1,19 @@
+#colors module in bash
+
+c_base01='\033[1;32m'
+c_base00='\033[1;33m'
+c_base0='\033[1;34m'
+c_base1='\033[1;36m'
+c_base2='\033[0;37m'
+c_base3='\033[1;37m'
+c_yellow='\033[0;33m'
+c_orange='\033[1;31m'
+c_red='\033[0;31m'
+c_magenta='\033[0;35m'
+c_violet='\033[1;35m'
+c_blue='\033[0;34m'
+c_cyan='\033[0;36m'
+c_green='\033[0;32m'
+c_reset='\033[0m'
+c_clear='\033[H\033[2J'
+
